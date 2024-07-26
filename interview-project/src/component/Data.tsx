21431b5279b24e56bd2b3a7f3bd0baf587e2f1bd
sstefanov <84671344+sstefanov19@users.noreply.github.com>
2024-07-26 23:21:16 +0300

    was bored added typescript

--- interview-project/src/component/Data.tsx
+++ interview-project/src/component/Data.tsx
@@ -1,7 +1,35 @@
 import { useState, useEffect } from "react";
 
-const Data = () => {
-  const [data, setData] = useState([]);
+interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: {
+    lat: string;
+    lng: string;
+  };
+}
+
+interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
+
+const Data: React.FC = () => {
+  const [data, setData] = useState<User[]>([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
