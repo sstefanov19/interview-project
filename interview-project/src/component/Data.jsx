c4f1e648e0983fce9b9d84341057e09bd9ffd4d4
sstefanov <84671344+sstefanov19@users.noreply.github.com>
2024-07-26 23:15:46 +0300

    variable name changes

--- interview-project/src/component/Data.jsx
+++ interview-project/src/component/Data.jsx
@@ -29,17 +29,17 @@ const Data = () => {
             <p className="text-gray-600">Loading data...</p>
           ) : (
             <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full">
-              {data.map((item) => (
-                <li key={item.id} className="bg-white p-5 rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300">
-                  <h2 className="text-xl font-semibold text-gray-800 mb-2">Name: {item.name}</h2>
-                  <p className="text-gray-700 mb-1"><span className="font-bold">Username:</span> {item.username}</p>
-                  <p className="text-gray-700 mb-1"><span className="font-bold">Email:</span> {item.email}</p>
-                  <p className="text-gray-700 mb-1"><span className="font-bold">Company:</span> {item.company.name}</p>
-                  <p className="text-gray-700 mb-1"><span className="font-bold">City:</span> {item.address.city}</p>
-                  <p className="text-gray-700 mb-1"><span className="font-bold">Street:</span> {item.address.street}</p>
-                  <p className="text-gray-700 mb-1"><span className="font-bold">Zipcode:</span> {item.address.zipcode}</p>
-                  <p className="text-gray-700 mb-1"><span className="font-bold">Phone:</span> {item.phone}</p>
-                  <p className="text-gray-700 mb-1"><span className="font-bold">Website:</span> {item.website}</p>
+              {data.map((user) => (
+                <li key={user.id} className="bg-white p-5 rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300">
+                  <h2 className="text-xl font-semibold text-gray-800 mb-2">Name: {user.name}</h2>
+                  <p className="text-gray-700 mb-1"><span className="font-bold">Username:</span> {user.username}</p>
+                  <p className="text-gray-700 mb-1"><span className="font-bold">Email:</span> {user.email}</p>
+                  <p className="text-gray-700 mb-1"><span className="font-bold">Company:</span> {user.company.name}</p>
+                  <p className="text-gray-700 mb-1"><span className="font-bold">City:</span> {user.address.city}</p>
+                  <p className="text-gray-700 mb-1"><span className="font-bold">Street:</span> {user.address.street}</p>
+                  <p className="text-gray-700 mb-1"><span className="font-bold">Zipcode:</span> {user.address.zipcode}</p>
+                  <p className="text-gray-700 mb-1"><span className="font-bold">Phone:</span> {user.phone}</p>
+                  <p className="text-gray-700 mb-1"><span className="font-bold">Website:</span> {user.website}</p>
                 </li>
               ))}
             </ul>
